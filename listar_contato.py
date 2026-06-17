666a69af4baa2def81d4249b98519d662e92863b
borguinny <kkkk@mail>
2026-06-17 19:11:27 -0300

    update

--- listar_contato.py
+++ listar_contato.py
@@ -10,12 +10,10 @@ nome_pesquisado = input("Nome: ")
 for registro in registros:
     nome, data_nascimento, telefone, endereco, bairro, genero = registro.strip().split(",")
 
-    if (nome == nome_pesquisado):
+    if nome_pesquisado.lower() in nome.lower():
+        print()
         print(f"{nome}")
         print(f"Data Nascimento:{data_nascimento}")
         print(f"Fone:{telefone}")
         print(f"End:{endereco} - Bairro: {bairro}")
         print(f"Gênero:{genero}")
-        print("-------------------------------------------")
-      
-    
